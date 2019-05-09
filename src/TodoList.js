215187a97ee7ec11de0a013c466dad6c0c8523b0
Helton Denis <heltondeniss@gmail.com>
2019-05-08 21:50:31 -0300

    add reducer, todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const TodoList = ({ todos, addTodo }) => (
-    <ul>
-        {props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
-    </ul>
+    <Fragment>
+    <ul>{todos.map(todo => <li key={todo.id}>{todo.text}</li>)}</ul>
+    <button onClick={() => addTodo('Fazer todo')}>Adicionar</button>
+    </Fragment>
 );
 
 TodoList.propTypes = {
+    addTodo: PropTypes.func.isRequired,
     todos: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number,
         text: PropTypes.string,
