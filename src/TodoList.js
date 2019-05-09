eee730b53ae386cd4a56c5c67353ee7d26e9cb22
Helton Denis <heltondeniss@gmail.com>
2019-05-08 22:06:56 -0300

    Add actions for remove todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,41 @@
-import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import * as TodoActions from "./store/actions/todos";
 
-const TodoList = ({ todos, addTodo }) => (
-    <Fragment>
-    <ul>{todos.map(todo => <li key={todo.id}>{todo.text}</li>)}</ul>
-    <button onClick={() => addTodo('Fazer todo')}>Adicionar</button>
-    </Fragment>
+const TodoList = ({ todos, addTodo, removeTodo }) => (
+  <Fragment>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => removeTodo(todo.id)}>Remover</button>
+        </li>
+      ))}
+    </ul>
+    <button onClick={() => addTodo("Fazer todo")}>Adicionar</button>
+  </Fragment>
 );
 
 TodoList.propTypes = {
-    addTodo: PropTypes.func.isRequired,
-    todos: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number,
-        text: PropTypes.string,
-    })).isRequired,
+  addTodo: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      text: PropTypes.string
+    })
+  ).isRequired
 };
 
 const mapStateToProps = state => ({
-    todos: state.todos,
+  todos: state.todos
 });
 
-const mapDispatchToProps = dispatch => ({
-    addTodo: text => dispatch({ type: 'ADD_TODO', payload: { text } }),
-});
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodoActions, dispatch);
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(TodoList); //High order component
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList); //High order component
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,20 +1,21 @@
 const INITIAL_STATE = [
-
-    {
-        id: 1,
-        text: 'Teste1'
-    },
-    {
-        id: 2,
-        text: 'Teste2'
-    }
+  {
+    id: 1,
+    text: "Teste1"
+  },
+  {
+    id: 2,
+    text: "Teste2"
+  }
 ];
 
 export default function todos(state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [...state, { id: Math.random(), text: action.payload.text }]
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, { id: Math.random(), text: action.payload.text }];
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.payload.id);
+    default:
+      return state;
+  }
 }
