4527078714af28dc6a5f827996ae3489228dd0fa
Helton Denis <heltondeniss@gmail.com>
2019-05-09 22:14:15 -0300

    actions favorites and reactatron

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
+import "./config/ReactotronConfig";
 
 import store from "./store";
-import TodoList from './TodoList';
+import TodoList from "./TodoList";
+
+console.tron.log("testeando");
 
 const App = () => (
   <Provider store={store}>
-    <TodoList></TodoList>
+    <TodoList />
   </Provider>
 );
 
